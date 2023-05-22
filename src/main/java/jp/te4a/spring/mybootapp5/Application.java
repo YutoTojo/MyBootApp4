f53c6abbb7ff73bd5623166d6ed81210c1717b78
200639 <200639@e0601-13.JC-21.local>
2023-05-22 09:54:24 +0900

    kadai5

--- src/main/java/jp/te4a/spring/mybootapp5/Application.java
+++ src/main/java/jp/te4a/spring/mybootapp5/Application.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.mybootapp4;
+package jp.te4a.spring.mybootapp5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
