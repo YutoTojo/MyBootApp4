768a5caa903ceb1aa26bad86b74078e6a1e67f49
200639 <200639@e0601-13.JC-21.local>
2023-05-23 12:36:33 +0900

    kadai5

--- src/main/java/jp/te4a/spring/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/mybootapp5/HelloController.java
@@ -1,8 +1,13 @@
 package jp.te4a.spring.mybootapp5;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HelloController {
 	
 	@RequestMapping(value="/post", method=RequestMethod.POST)
