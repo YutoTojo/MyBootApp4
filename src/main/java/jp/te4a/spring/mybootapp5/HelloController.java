dd40523eb9c5a0db101df2ac85880f832e712b67
200639 <200639@e0601-13.JC-21.local>
2023-05-23 12:29:55 +0900

    kadai5

--- src/main/java/jp/te4a/spring/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/mybootapp5/HelloController.java
@@ -3,37 +3,13 @@ package jp.te4a.spring.mybootapp5;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(value="/post", method=RequestMethod.POST)
-public ModelAndView postForm(@RequestParam("text1") String text1) {
-    ModelAndView mv = new ModelAndView("index");
-    mv.addObject("msg", "you write'" + text1 + "'!!!");
-    return mv;
-}
-
-/*3
-@RequestMapping(value="/post", method=RequestMethod.POST)
-public ModelAndView postForm(@RequestParam("text1") String text1) {
-    ModelAndView mv = new ModelAndView("index");
-    mv.addObject("msg", "you write '" + text1 + "'!!!");
-    return mv;
-}
-*/
-
-/*2
-@RequestMapping("/")
-public String index(Model model) {
-    model.addAttribute("msg", "this is  a setting message");
-    return "index";
-}
- */
-
-/*1
-@Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
-    	//return "this is Spring Boot sample.";
-        return "index";
-    }
+	
+	@RequestMapping(value="/post", method=RequestMethod.POST)
+	public ModelAndView postForm(@RequestParam("text1") String text1) {
+	    ModelAndView mv = new ModelAndView("index");
+	    mv.addObject("msg", "you write '" + text1 + "'!!!");
+	    return mv;
+	}
+	
 }
-*/
